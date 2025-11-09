4ee94f3d0695e9a879995df35178e175ea1e7b29
Eric Via <evia002@ucr.edu>
2025-11-09 00:10:13 -0800

    Added modified nearest neighbor

--- NN.h
+++ NN.h
@@ -32,6 +32,7 @@ class k_means{
     //double euclidean(int i, int j);
     void write_route_to_file(const string &inputFilename, int chosenNumClusters);
     void nearest_neighbor_distance(vector<vector<tuple<int,double,double>>> IndividualClusters);
+    void modified_nearest_neighbor_distance(vector<vector<tuple<int,double,double>>> IndividualClusters);
     //double modified_nearest_neighbor_distance(double p);
     
     void kMeansClustering();
@@ -330,6 +331,7 @@ void k_means::write_route_to_file(const string &inputFilename, int chosenNumClus
 //     }
 // }
 
+
 void k_means::nearest_neighbor_distance(vector<vector<tuple<int,double,double>>> IndividualClusters) {
 
     double total_distance_all_clusters = 0.0;
@@ -375,6 +377,82 @@ void k_means::nearest_neighbor_distance(vector<vector<tuple<int,double,double>>>
                 visited[next_tree] = true;
                 current_tree = next_tree;
             }
+        }
+
+        // Close the loop by returning to the center
+        double dx = get<1>(cluster[current_tree]) - get<1>(cluster[0]);
+        double dy = get<2>(cluster[current_tree]) - get<2>(cluster[0]);
+        cluster_distance += sqrt(dx*dx + dy*dy);
+        cluster_route[cluster_size] = get<0>(cluster[0]); // append center at end
+
+        total_distance_all_clusters += cluster_distance;
+        tempRoute.push_back(vector<int>(cluster_route, cluster_route + cluster_size + 1));
+        clusterDistances.push_back(cluster_distance);
+    }
+
+    if(bestRouteDistance == 0 || bestRouteDistance > total_distance_all_clusters){
+        bestRouteDistance = total_distance_all_clusters;
+        clusterRoute = tempRoute;
+    }
+}
+
+void k_means::modified_nearest_neighbor_distance(vector<vector<tuple<int,double,double>>> IndividualClusters) {
+    int p = 0.10;
+    double total_distance_all_clusters = 0.0;
+    vector<vector<int>> tempRoute;
+    clusterDistances.clear();
+
+    for(int c = 0; c < IndividualClusters.size(); ++c){
+        vector<tuple<int,double,double>> cluster = IndividualClusters[c];
+        int cluster_size = cluster.size();
+
+        if(cluster_size <= 1) continue;
+
+        bool visited[4096] = {false};    
+        double cluster_distance = 0.0;
+        int cluster_route[4096];
+
+        // Start at the cluster center (first element)
+        int current_tree = 0;
+        cluster_route[0] = get<0>(cluster[current_tree]);
+        visited[current_tree] = true;
+
+        // iterate over all nodes including center
+        for (int i = 1; i < cluster_size; ++i) {
+            int next_tree = -1;
+            int second_best_tree = -1;
+            double bestDist = 100000;
+            double secondBestDist = 100000;
+
+            for (int j = 0; j < cluster_size; ++j) {
+                if (!visited[j]) {
+                    double dx = get<1>(cluster[current_tree]) - get<1>(cluster[j]);
+                    double dy = get<2>(cluster[current_tree]) - get<2>(cluster[j]);
+                    double dist = sqrt(dx*dx + dy*dy);
+
+                    if(dist < bestDist){
+                        secondBestDist = bestDist;
+                        second_best_tree = next_tree;
+
+                        bestDist = dist;
+                        next_tree = j;
+                    } else if (dist < secondBestDist){
+                        secondBestDist = dist;
+                        second_best_tree = j;
+                    }
+                }
+            }
+
+            if(second_best_tree != -1 && ((double)rand() / RAND_MAX)< p){
+                next_tree = second_best_tree;
+                cluster_distance += second_best_tree;
+            } else {
+                cluster_distance += bestDist;
+            }
+            cluster_route[i] = get<0>(cluster[next_tree]);
+            visited[next_tree] = true;
+            current_tree = next_tree;
+            
         }
 
         // Close the loop by returning to the center
